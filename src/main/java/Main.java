a888940485c142f18d217c9a29fadb88c0974694
Riccardo Posa <riccardo.posa@student.supsi.ch>
2021-04-04 16:37:57 +0200

    Friendly message

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,7 +8,7 @@ import java.text.ParseException;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException, ParseException {
+    public static void main(String[] args) {
         File target = new File(args[0]);
         if(!target.exists()){
             System.out.println("File not found");
@@ -18,11 +18,17 @@ public class Main {
         if(target.isDirectory()){
             File[] files= target.listFiles();
             for(File f : files){
-                new InstanceWriter(instanceReader.read(f)).write();
+                try {
+                    new InstanceWriter(instanceReader.read(f)).write();
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         } else if(target.isFile()){
 
         } else return;
-
+        System.out.println("Conversion completed");
     }
 }
