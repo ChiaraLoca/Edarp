9e475b91780d183d4dc843c69642b9441bda624e
Chiara <Chiara.locatelli.395@gmail.com>
2021-04-12 15:24:49 +0200

    Fix Mission

--- src/main/java/model/Mission.java
+++ src/main/java/model/Mission.java
@@ -1,10 +1,14 @@
 package model;
 
 public class Mission {
-    Node departure;//i
-    Node arrival;//j
-    double departureTime;
-    double arrivalTime;
+    Node startingNode;//i
+    Node arrivingNodo;//j
+    double departureTime;//T[i]
+    double arrivalTime;//T[j]
+    double startingNodeArrivingTime; //arr[i]
+    double startingNodeDepartureTime; //dep[i]
+    double arrivingNodeArrivingTime;  //arr[j]
+    double arrivingNodeDepartureTime;//dep[j]
     double travelTime;//t[i,j]
     double batteryLevel;//i
     double chargingTime;//i
