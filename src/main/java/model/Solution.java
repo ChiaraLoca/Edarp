a97d45acd32eb45dbcd21613e8b6742ef79ba407
Samuele Dell'Oca <samuele.delloca@brandmind.ch>
2021-04-16 18:45:07 +0200

    Added decision variables definition

--- src/main/java/model/Solution.java
+++ src/main/java/model/Solution.java
@@ -15,6 +15,14 @@ public class Solution {
     private Instance instance;
     private List<Mission> list;
 
+    // Decision variables:
+    private int[][][] vehicleSeqStopAtLocations; // X: 1 if vehicle k sequentially stops at location i and j ∈ V, 0 otherwise
+    private double[][] timeVehicleStartsAtLocation; // T: time at which vehicle k starts its service at location i ∈ V
+    private double[][] loadOfVehicleAtLocation; // L: load of vehicle k at location i ∈ V
+    private double[][] batteryLoadOfVehicleAtLocation; // B: battery load of vehicle k at location i ∈ V
+    private double[][] chargingTimeOfVehicleAtStation; // E: charging time of vehicle k at charging station s ∈ S
+    private double[][] excessRideTimeOfPassenger; // R: excess ride-time of passenger i ∈ P
+
     public Solution(Instance instance) {
         this.instance = instance;
         this.list= new ArrayList<>();
