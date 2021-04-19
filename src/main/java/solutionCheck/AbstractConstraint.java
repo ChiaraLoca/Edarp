daeb4602b67a4e458061e348132cb3a288ac7d1a
Samuele Dell'Oca <samuele.delloca@brandmind.ch>
2021-04-19 14:19:02 +0200

    feat: union and diff method

--- src/main/java/solutionCheck/AbstractConstraint.java
+++ src/main/java/solutionCheck/AbstractConstraint.java
@@ -2,6 +2,8 @@ package solutionCheck;
 
 import model.Solution;
 
+import java.util.*;
+
 public abstract class AbstractConstraint {
     protected Solution solution;
 
@@ -10,4 +12,37 @@ public abstract class AbstractConstraint {
     }
 
     abstract boolean check();
+
+    protected static Integer[] arrayUnion(int[] ... param) {
+        Set<Integer> set=new HashSet<>();
+        for(int[] p:param) {
+            set.addAll(Arrays.asList(toInteger(p)));
+        }
+        Integer[] unionArray = set.toArray(new Integer[set.size()]);
+        Arrays.sort(unionArray);
+        return unionArray;
+    }
+
+    protected static Integer[] arrayDiff(int[] ... param) {
+        List<Integer> list=new ArrayList<>();
+        for(int[] p:param) {
+            list.addAll(Arrays.asList(toInteger(p)));
+        }
+        List<Integer> currencyList=new ArrayList<>();
+        for(Integer i:list) {
+            int occ=Collections.frequency(list,i);
+            if(occ==1)
+                currencyList.add(i);
+        }
+        return currencyList.toArray(new Integer[currencyList.size()]);
+    }
+
+    protected static Integer[] toInteger(int[] a) {
+        Integer[] arr=new Integer[a.length];
+        for(int i=0; i<a.length;i++) {
+            arr[i]=a[i];
+        }
+        return arr;
+    }
+
 }
